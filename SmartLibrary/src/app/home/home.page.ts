164f705310efa1f834e26aedcad11b38a5754c7a
Ciara Gilsenan <ciaragil98@gmail.com>
2022-02-28 16:22:47 +0000

    started ble code, need to test with esp32

--- SmartLibrary/src/app/home/home.page.ts
+++ SmartLibrary/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BleClient } from '@capacitor-community/bluetooth-le';
+import { BLE } from '@ionic-native/ble';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  devices:any[] = [];
+  scanStatus:String = '';
+  device:String = '';
 
   constructor() {}
 
+  async scan() {
+    this.devices = [];
+    try{
+      await BleClient.initialize();
+      await BleClient.requestLEScan(
+        {
+          services: [],
+        },
+        (result) => {
+          this.scanStatus = 'Scanning';
+          console.log("Scan ", result);
+          this.devices = result[0];
+        }
+      );
+      setTimeout(async () =>{
+        await BleClient.stopLEScan();
+        console.log("Stop Scanning");
+        this.scanStatus = 'Done Scanning';
+      }, 10000);
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
+  anotherScan(){
+
+  }
 }
