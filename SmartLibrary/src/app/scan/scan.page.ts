3e150ce722df54a641c4d4db8444df6108af5a0b
Maddie <maddie.comtois@gmail.com>
2022-03-01 13:11:55 +0000

    Update scan.page.ts

--- SmartLibrary/src/app/scan/scan.page.ts
+++ SmartLibrary/src/app/scan/scan.page.ts
@@ -10,8 +10,8 @@ import { Platform } from '@ionic/angular';
 export class ScanPage implements OnInit {
   
   private platform: Platform;
-  readerMode$;
-  scanResponse = "no response"
+  private readerMode$;
+  private scanResponse = "no response"
 
   constructor(private nfc: NFC, private ndef: Ndef, platform: Platform) {
     this.platform = platform;
@@ -24,20 +24,20 @@ export class ScanPage implements OnInit {
     // Read NFC Tag - iOS
     // On iOS, a NFC reader session takes control from your app while scanning tags then returns a tag
     if(this.platform.is('ios')) {
-      console.log("this is read")
+      console.log("ios detected")
       try {
           let tag = await this.nfc.scanNdef();
           console.log(JSON.stringify(tag));
           this.scanResponse = JSON.stringify(tag);
        } catch (err) {
-           console.log('Error reading tag', err);
+           this.scanResponse = 'Error reading tag'
        }
     }
     
     // Read NFC Tag - Android
     // Once the reader mode is enabled, any tags that are scanned are sent to the subscriber
     if(this.platform.is('android')) {
-      console.log("android is read")
+      console.log("android detected")
        let flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
        this.readerMode$ = this.nfc.readerMode(flags).subscribe(
            tag => this.scanResponse = JSON.stringify(tag),
