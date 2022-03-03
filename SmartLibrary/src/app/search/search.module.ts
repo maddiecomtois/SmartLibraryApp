40db0aa69e4ed5375c4e27970d9d46d074f1fbfb
Maddie <maddie.comtois@gmail.com>
2022-03-03 12:26:44 +0000

    add search bar functionality

--- SmartLibrary/src/app/search/search.module.ts
+++ SmartLibrary/src/app/search/search.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -14,7 +15,7 @@ import { SearchPage } from './search.page';
     FormsModule,
     IonicModule,
     SearchPageRoutingModule,
-    ReactiveFormsModule
+    Ng2SearchPipeModule
   ],
   declarations: [SearchPage]
 })
--- SmartLibrary/src/app/search/search.page.ts
+++ SmartLibrary/src/app/search/search.page.ts
@@ -1,55 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable, combineLatest } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
 import { BookService } from '../services/book.service';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss']
 })
 export class SearchPage implements OnInit {
-  public searchField: FormControl;
-  public foodList$: Observable<FoodItem[]>;
   public bookList: Book[];
+  public searchTerm: string;
 
-
-
-  constructor(private bookSerice: BookService) {
-    this.searchField = new FormControl('');
-
+  constructor(private bookService: BookService) {
   }
 
-  async ngOnInit() {
-    const searchTerm$ = this.searchField.valueChanges.pipe(
-      startWith(this.searchField.value)
-    );
-    this.bookSerice.getBooks().subscribe((result:Book[]) => {
+  ngOnInit() {
+    this.bookService.getBooks().subscribe((result:Book[]) => {
       console.log(result);
       this.bookList = result;
     });
-
-    /*
-    // tutorial: https://jsmobiledev.com/article/searchbar-firebase/
-    const foodList$ = Observable.create(this.foods);
-
-    this.foodList$ = combineLatest([foodList$, searchTerm$]).pipe(
-      map(([foodList, searchTerm]) =>
-        foodList.filter(
-          (foodItem) =>
-            searchTerm === '' ||
-            foodItem.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-      )
-    );
-    */
-
   }
-
-}
-
-interface FoodItem {
-  name: string;
 }
 
 export interface Book {
