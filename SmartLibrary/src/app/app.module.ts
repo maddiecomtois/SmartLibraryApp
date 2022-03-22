7367438a9bfb8b586881453fd6858020db77c874
Chaitanya Joglekar <chaitanyajoglekar2@gmail.com>
2022-03-22 23:37:07 +0000

    Adding custom cordova BLE Plugin

--- SmartLibrary/src/app/app.module.ts
+++ SmartLibrary/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { BLE } from '@ionic-native/ble/ngx';
+import {SmartBeaconPlugin} from '@ionic-native/smart-beacon-plugin/ngx'
 //import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/ngx';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx'
 
@@ -18,7 +19,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
   HttpClientModule],
-  providers: [BLE, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },BookService],
+  providers: [BLE, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },BookService,SmartBeaconPlugin],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
